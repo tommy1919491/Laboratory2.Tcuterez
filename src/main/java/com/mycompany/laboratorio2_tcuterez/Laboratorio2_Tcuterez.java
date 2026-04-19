2e37a96c0d842d5085a9440796a64549b7b9c88d
tomy1905 <Tcuterezp@miumg.edu.gt>
2026-04-18 18:32:35 -0600

    primer guardado

---
+++ src/main/java/com/mycompany/laboratorio2_tcuterez/Laboratorio2_Tcuterez.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laboratorio2_tcuterez;
+
+/**
+ *
+ * @author cuter
+ */
+public class Laboratorio2_Tcuterez {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
